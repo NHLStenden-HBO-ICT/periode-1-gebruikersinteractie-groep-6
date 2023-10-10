7f3f564f100a333e97b56bbe949633de136e6cd4
Virtual <officialimvirtual@gmail.com>
2023-10-10 15:32:46 +0200

    Commit base structure (excl. Windows folder)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace periode_1_gebruikersinteractie_groep_6
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		// create a new instance of the gamecore class
+		private Logic.GameCore gameCore = new Logic.GameCore();
+		public App()
+		{
+			// create a new instance of the counter class
+			Classes.Counter counter = new Classes.Counter();
+			// call the create function on the gamecore class
+			gameCore.Create(counter);
+		}
+	}
 }
