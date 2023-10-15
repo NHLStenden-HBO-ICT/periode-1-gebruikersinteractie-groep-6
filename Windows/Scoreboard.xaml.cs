fdae8e30cf73de22f52a8a62a1663b055221ca9f
Virtual <officialimvirtual@gmail.com>
2023-10-16 00:00:29 +0200

    push fade & music fix

--- Windows/Scoreboard.xaml.cs
+++ Windows/Scoreboard.xaml.cs
@@ -7,11 +7,11 @@ namespace periode_1_gebruikersinteractie_groep_6.Windows
 	/// </summary>
 	public partial class Scoreboard : UserControl
 	{
-		private ContentControl parent;
-		public Scoreboard(ContentControl parent)
+		private Main parent;
+		public Scoreboard(Main parent)
 		{
-            InitializeComponent();
-            this.parent = parent;
-        }
+			InitializeComponent();
+			this.parent = parent;
+		}
 	}
 }
