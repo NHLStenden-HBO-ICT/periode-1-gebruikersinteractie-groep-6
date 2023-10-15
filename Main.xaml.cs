f2712a10ffa04191219081798e9887666da03302
Virtual <officialimvirtual@gmail.com>
2023-10-15 16:47:58 +0200

    Make opscreen, startscreen functional & restructure windows to use usercontrols for more dynamic window switching. TODO: add animations

---
+++ Main.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace periode_1_gebruikersinteractie_groep_6.Windows
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Main : Window
+	{
+		public Main()
+		{
+			InitializeComponent();
+			Opscreen opscreen = new Opscreen(this);
+			Startscherm Startscherm = new Startscherm();
+			contentControl.Content = opscreen;
+
+			var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(3) };
+			timer.Start();
+			timer.Tick += (sender, args) =>
+			{
+				timer.Stop();
+				contentControl.Content = Startscherm;
+			};
+		}
+
+		public void ChangeContent(UserControl newContent)
+		{
+			contentControl.Content = newContent;
+		}
+	}
+}
