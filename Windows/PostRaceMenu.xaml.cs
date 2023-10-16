8593e34a195f8d73438af1cb4de502770d629177
Virtual <officialimvirtual@gmail.com>
2023-10-16 23:30:25 +0200

    fixes voor bugs, maak slider werkend

--- Windows/PostRaceMenu.xaml.cs
+++ Windows/PostRaceMenu.xaml.cs
@@ -1,15 +1,37 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace periode_1_gebruikersinteractie_groep_6.Windows
 {
 	/// <summary>
-	/// Interaction logic for PostRaceMenu.xaml
+	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class PostRaceMenu : UserControl
 	{
-		public PostRaceMenu()
+		private Main parent;
+		public PostRaceMenu(Main parent)
 		{
 			InitializeComponent();
+			this.parent = parent;
+		}
+
+		private void openMainMenu(object sender, RoutedEventArgs e)
+		{
+			instellingenUI instellingenUI = new instellingenUI(this.parent);
+			parent.ChangeContent(instellingenUI);
+		}
+
+		private void openScoreboard(object sender, RoutedEventArgs e)
+		{
+			Scoreboard scoreboard = new Scoreboard(this.parent);
+			parent.ChangeContent(scoreboard);
+		}
+
+		private void openPreRaceMenu(object sender, RoutedEventArgs e)
+		{
+			PreRaceMenu PreRaceMenu = new PreRaceMenu(this.parent);
+			parent.ChangeContent(PreRaceMenu);
 		}
 	}
 }
+
