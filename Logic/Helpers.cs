6ddd93f22cc785720a6cb8f446d52febe4820544
daeleexd <blomdaenlee@gmail.com>
2023-10-16 10:24:19 +0200

    Logic/helpers, its going to cause conflicts for sure

--- Logic/Helpers.cs
+++ Logic/Helpers.cs
@@ -6,20 +6,5 @@ namespace periode_1_gebruikersinteractie_groep_6.Logic
 {
     public class Helpers
     {
-        public static SoundPlayer currentMusic;
-
-        public static void PlayMusic(string path)
-        {
-            if (currentMusic != null)
-            {
-                currentMusic.Stop();
-                currentMusic.Dispose();
-                currentMusic = null;
-            }
-
-            currentMusic = new SoundPlayer(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", path));
-            currentMusic.Load();
-            currentMusic.PlayLooping();
-        }
     }
 }
