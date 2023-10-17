91f88d3aa81211a14759282072e49abcfc5313d9
daeleexd <blomdaenlee@gmail.com>
2023-10-17 15:52:47 +0200

    Audio loops now, and added a viewbox in mainMenu

--- Logic/Helpers.cs
+++ Logic/Helpers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace periode_1_gebruikersinteractie_groep_6
 {
@@ -12,7 +13,8 @@ namespace periode_1_gebruikersinteractie_groep_6
 
 		public static void PlayMusic(string path)
 		{
-			if (currentMusic != null)
+
+            if (currentMusic != null)
 			{
 				currentMusic.Stop();
 				currentMusic.Close();
@@ -22,7 +24,20 @@ namespace periode_1_gebruikersinteractie_groep_6
 			currentMusic = new MediaPlayer();
 			currentMusic.Open(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", path)));
 			currentMusic.Play();
-		}
+
+            
+			currentMusic.MediaEnded += (sender, e) =>
+			{
+                currentMusic.Position = TimeSpan.Zero;
+                currentMusic.Play();
+            };	
+
+            var timeLine = new MediaTimeline(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", path)));
+            timeLine.RepeatBehavior = RepeatBehavior.Forever;
+            var mediaPlayer = new MediaPlayer();
+            mediaPlayer.Clock = timeLine.CreateClock();
+            mediaPlayer.Clock.Controller.Begin();
+        }
 
 		public static void Mute(bool? toMute)
 		{
